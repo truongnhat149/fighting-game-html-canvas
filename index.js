15d15a7b59ad4dbcf3d99f9c1bb69d9d22670783
Truong <vjc.ichi.149@gmail.com>
2022-06-29 09:45:17 +0700

    project init

---
+++ index.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector('canvas');
+const canvasContext = canvas.getContext('2d');
+
+canvas.width = 1024;
+canvas.height = 576;
+
+canvasContext.fillRect(0, 0, canvas.width, canvas.height);
